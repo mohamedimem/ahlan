9fa210a0bf0569730c530948538eb22d2e67b33c
hmmmidk00 <imem.hamdi@etudiant-isi.utm.tn>
2023-05-21 23:30:40 +0100

    lll

---
+++ lib/ajouteraliment/utils/mekla_argument.dart
@@ -0,0 +1,7 @@
+class AjouterAlimentArguments {
+  final String nomMekla;
+  final double quantity;
+  final double lipides;
+
+  AjouterAlimentArguments(this.nomMekla, this.quantity, this.lipides);
+}
---
+++ lib/quantite.dart
@@ -0,0 +1,342 @@
+import 'package:flutter_application_7/mekla/mekla.dart';
+import 'package:flutter_application_7/mesure_sucre.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+import 'ajouteraliment/utils/mekla_argument.dart';
+import 'mekla/utils/mekla_argument.dart';
+
+class Quantite extends StatefulWidget {
+  final double? ajouter;
+
+  Quantite({required this.ajouter});
+
+  @override
+  State<StatefulWidget> createState() => QuantiteState();
+}
+
+class QuantiteState extends State<Quantite> {
+  MesureState mesureState = MesureState();
+  final _valeurcontroller = TextEditingController();
+
+  double replaceGlucides = 0.0;
+  double number = 0.0;
+  double result = 0.0;
+  double finalResult = 0.0;
+
+  void calculateur() {
+    number = double.parse(_valeurcontroller.text);
+    setState(() {
+      result = (number / 10);
+    });
+  }
+
+  var c = Color(0xFFCE6D39);
+  var d = Color(0xff011638);
+  double val = 0.0;
+  valCircle(result) {
+    val = result * 0.005;
+    return val;
+  }
+
+  double lipides = 0.0;
+  // final graisse = 20;
+  Lipides(double number) {
+    setState(() {
+      lipides = ((number * 2) / 10);
+    });
+    return lipides;
+  }
+
+  double insulineresult = 0.0;
+
+  double insuline(double result, double ajouter) {
+    setState(() {
+      insulineresult = ((result * ajouter) / 10);
+    });
+    return insulineresult;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void saveNameAndQuantityToSharedPreferences(
+      String name, double quantity, double quantity2) async {
+    final SharedPreferences _preferences =
+        await SharedPreferences.getInstance();
+
+    List<Map<String, dynamic>> itemList = _preferences
+            .getStringList('itemList')
+            ?.map((item) => Map<String, dynamic>.from(json.decode(item)))
+            ?.toList() ??
+        [];
+
+    Map<String, dynamic> newItem = {
+      'name': name,
+      'quantity': quantity,
+      'quantity2': quantity2,
+    };
+
+    itemList.add(newItem);
+
+    await _preferences.setStringList(
+      'itemList',
+      itemList.map((item) => json.encode(item)).toList(),
+    );
+  }
+
+  void printItemListFromSharedPreferences() async {
+    final SharedPreferences _preferences =
+        await SharedPreferences.getInstance();
+
+    List<Map<String, dynamic>> itemList = _preferences
+            .getStringList('itemList')
+            ?.map((item) => Map<String, dynamic>.from(json.decode(item)))
+            ?.toList() ??
+        [];
+
+    for (var item in itemList) {
+      print(
+          'Name: ${item['name']}, Quantity: ${item['quantity']},Quantity2: ${item['quantity2']}');
+      print('----------');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as MeklaArgument;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff011638),
+        title: Text(
+          "Quantité ${args.nomMekla}",
+          style: TextStyle(fontSize: 18, color: Color(0xFFCE6D39)),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    'Quantité en gramme: ',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: d,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  //height: 80,
+                  width: 300,
+                  alignment: Alignment.bottomLeft,
+                  child: TextField(
+                    //padding: EdgeInsets.all(16.0),
+                    controller: _valeurcontroller,
+                    decoration: InputDecoration(
+                      hintText: ' quantité ',
+                      hintStyle:
+                          TextStyle(fontStyle: FontStyle.italic, fontSize: 20),
+                      suffixStyle: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        fontSize: 40,
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+                SizedBox(
+                  width: 12,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              width: 220,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: c,
+                  shape: StadiumBorder(),
+                ),
+                child: const Text(
+                  'Enregistrer',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color(0xff011638),
+                  ),
+                ),
+                onPressed: () {
+                  setState(() {
+                    calculateur();
+                    Lipides(number);
+
+                    saveNameAndQuantityToSharedPreferences(
+                        args.nomMekla, result, lipides);
+                    //wait
+                    Future.delayed(Duration(milliseconds: 10000));
+                    // Navigator.pushNamed(context, '/mekla');
+                    Navigator.pushNamed(
+                      context,
+                      '/ajouteraliment',
+                    );
+
+                    // finalResult = insuline(result, widget.ajouter!);
+                  });
+                },
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              // transform: Matrix4.rotationZ(0.09),
+              //color: Color(0xffFFEEE4),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    height: 250,
+                    alignment: Alignment.bottomCenter,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(42)),
+                      color: Color(0xffFFEEE4),
+                    ),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Image.network(
+                          "https://cdn-icons-png.flaticon.com/512/2396/2396713.png",
+                          height: 50,
+                          width: 50,
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        CircularPercentIndicator(
+                          footer: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Text("Glucides (g)",
+                                style: TextStyle(
+                                  color: Color(0xFFCE6D39),
+                                  fontSize: 15,
+                                )),
+                          ),
+                          animation: true,
+                          animationDuration: 1050,
+                          radius: 40,
+                          lineWidth: 10.0,
+                          percent: valCircle(result),
+                          progressColor: Color(0xFFCE6D39),
+                          backgroundColor: Color(0xF8011638),
+                          circularStrokeCap: CircularStrokeCap.round,
+                          center: Text("$result",
+                              style: TextStyle(
+                                  color: Color(0xFFCE6D39), fontSize: 20)),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  /* CircularPercentIndicator(
+                    footer:
+                        Text("Insuline", style: TextStyle(color: c, fontSize: 15)),
+                    animation: true,
+                    animationDuration: 1050,
+                    radius: 45,
+                    lineWidth: 12.0,
+                    percent: valCircle(result),
+                    progressColor: Color(0xFFCE6D39),
+                    backgroundColor: Color(0xF8011638),
+                    circularStrokeCap: CircularStrokeCap.round,
+                    center: Text(
+                      "$insulineresult",
+                      style: TextStyle(fontSize: 20, color: d),
+                    ),
+                  ),*/
+
+                  Container(
+                    alignment: Alignment.bottomCenter,
+                    height: 250,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(42)),
+                      color: Color(0xffFFEEE4),
+                    ),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Image.network(
+                          "https://cdn-icons-png.flaticon.com/512/4264/4264699.png",
+                          height: 50,
+                          width: 50,
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        CircularPercentIndicator(
+                          footer: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Text("Lipides (g)",
+                                style: TextStyle(
+                                    color: Color(0xFFCE6D39), fontSize: 15)),
+                          ),
+                          animation: true,
+                          animationDuration: 1050,
+                          radius: 40,
+                          lineWidth: 10.0,
+                          percent: valCircle(result),
+                          progressColor: Color(0xFFCE6D39),
+                          backgroundColor: Color(0xF8011638),
+                          circularStrokeCap: CircularStrokeCap.round,
+                          center: Text(
+                            "$lipides",
+                            style: TextStyle(fontSize: 20, color: c),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
